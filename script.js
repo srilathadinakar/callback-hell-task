292a27f2ecedf5926539ab12a1ca3616f14b7af0
srilathadinakar <srilathadinakar@gmail.com>
2024-04-17 12:56:03 +0530

    completed

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("container").innerHTML="<b>10</b>"
+    setTimeout(()=>{
+        document.getElementById("container").innerHTML="<b>9</b>"
+        setTimeout(()=>{
+            document.getElementById("container").innerHTML="<b>8</b>"
+            setTimeout(()=>{
+                document.getElementById("container").innerHTML="<b>7</b>"
+                setTimeout(()=>{
+                    document.getElementById("container").innerHTML="<b>6</b>"
+                    setTimeout(()=>{
+                        document.getElementById("container").innerHTML="<b>5</b>"
+                        setTimeout(()=>{
+                            document.getElementById("container").innerHTML="<b>4</b>"
+                            setTimeout(()=>{
+                                document.getElementById("container").innerHTML="<b>3</b>"
+                                setTimeout(()=>{
+                                    document.getElementById("container").innerHTML="<b>2</b>"
+                                    setTimeout(()=>{
+                                        document.getElementById("container").innerHTML="<b>1</b>"
+                                        setTimeout(()=>{
+                                            document.getElementById("container").innerHTML="<b>Happy Independence Day</b>"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
